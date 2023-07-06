85f9702ef6223207fea7525ee90c26561d334c2e
Anthony <anthony@newtheory.is>
2023-07-06 11:42:59 -0400

    Added more dates to calendar

--- src/components/schedule.js
+++ src/components/schedule.js
@@ -7,13 +7,27 @@ import 'react-big-calendar/lib/css/react-big-calendar.css'
 const localizer = dayjsLocalizer(dayjs)
 
 const calendarStyles = {
-    marginTop: 40,
+    marginTop: 100,
     marginBottom: 40,
+    height: 500,
 }
 
 export default function MyCalendar(props) {
-  // function getBackgroundColor(props) {
-  //   console.log(props.style)
+
+  // function eventStyleGetter(event, start, end, isSelected) {
+  //   console.log(event);
+  //   var backgroundColor = '#' + event.hexColor;
+  //   var style = {
+  //       backgroundColor: backgroundColor,
+  //       borderRadius: '0px',
+  //       opacity: 0.8,
+  //       color: 'black',
+  //       border: '0px',
+  //       display: 'block'
+  //   };
+  //   return {
+  //       style: style
+  //   };
   // }
 
   return(
@@ -24,24 +38,9 @@ export default function MyCalendar(props) {
           events={myEventsList}
           startAccessor="start"
           endAccessor="end"
-          style={{ height: 500, marginTop: 100 }}
+          drilldownView={Views.DAY}
+          style={calendarStyles}
         />
       </div>
   );
 }
-
-// const MyCalendar = (props) => (
-  
-//   <div>
-//     <Calendar
-//       defaultView={Views.DAY}
-//       localizer={localizer}
-//       events={myEventsList}
-//       startAccessor="start"
-//       endAccessor="end"
-//       style={{ height: 500, marginTop: 100 }}
-//     />
-//   </div>
-// )
-
-// export default MyCalendar
